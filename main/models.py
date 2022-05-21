f234ed360e347580cdc299b0002efbe082d522c2
kirill <kirillefimovic141@gmail.com>
2022-05-21 16:19:25 +0300

    Updated local files

--- main/models.py
+++ main/models.py
@@ -10,7 +10,7 @@ class Product(models.Model):
     category_id = models.IntegerField()
     amount = models.IntegerField()
     availability = models.CharField(max_length=50)
-    img = models.ImageField()
+    img = models.ImageField(upload_to='static/images')
 
 
 class Category(models.Model):
