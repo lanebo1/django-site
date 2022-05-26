10de50367d6141f7157ff60dba1fdbef79f069fa
kirill <kirillefimovic141@gmail.com>
2022-05-26 10:36:39 +0300

    Updated local files, added stable version all sites, reg/login - done!

--- main/forms.py
+++ main/forms.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import User
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(label="Password", widget=forms.PasswordInput)
-    password2 = forms.CharField(label="Repeat password", widget=forms.PasswordInput)
 
 class Meta:
     model = User
