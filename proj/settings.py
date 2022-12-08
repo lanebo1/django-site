ae21db7da54516184c522528f99447131782c4d8
Kirill Efimovich <kirillefimovic141@gmail.com>
2022-12-08 13:26:26 +0300

    ANUS

--- proj/settings.py
+++ proj/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_jsonfield_backport',
     'main',
     'crispy_forms',
     'debug_toolbar',
