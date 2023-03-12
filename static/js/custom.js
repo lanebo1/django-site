f8b7ff68e7822a8bb809742f700129d1db8e3a1b
Kirill Efimovich <kirillefimovic141@gmail.com>
2023-03-13 00:03:15 +0300

    Stable version 1.0!

---
+++ static/js/custom.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+	"use strict";
+			$('#client').owlCarousel({
+				items:5,
+				loop:true,
+				smartSpeed: 1000,
+				autoplay:true,
+				dots:false,
+				autoplayHoverPause:true,
+				responsive:{
+						0:{
+							items:2
+						},
+						415:{
+							items:3
+						},
+						600:{
+							items:3
+
+						},
+						1200:{
+							items:5
+						}
+					}
+				});
+				$('.play').on('click',function(){
+					owl.trigger('play.owl.autoplay',[1000])
+				})
+				$('.stop').on('click',function(){
+					owl.trigger('stop.owl.autoplay')
+				})
+
+			$("#upperpart-carousel").owlCarousel({
+				items: 1,
+				autoplay: true,
+				loop: true,
+				dots:false,
+				mouseDrag:true,
+				nav:false,
+				smartSpeed:1000,
+				transitionStyle:"fade",
+				animateIn: 'fadeIn',
+				animateOut: 'fadeOutLeft'
+			});
+});
